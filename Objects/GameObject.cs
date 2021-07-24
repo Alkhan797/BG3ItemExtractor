b23d74b3dbf3c42c4c1ce40c036ac8dbe6fe25f1
alkhan <alkhan797@protonmail.com>
2021-07-24 18:58:01 +0200

    Initial Commit

---
+++ Objects/GameObject.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetAllItemsBG3.Objects
+{
+    public class GameObject
+    {
+        public string EntryName { get; protected set; }
+        public string Type { get; protected set; }
+        public string UsingReference { get; protected set; }
+        public Dictionary<string, string> Data { get; protected set; }
+
+        protected GameObject(
+            string name,
+            string type,
+            string usingReference,
+            Dictionary<string, string> data)
+        {
+            EntryName = name;
+            Type = type;
+            UsingReference = usingReference;
+            Data = data;
+        }
+
+        public static GameObject Create(string name, string type, string usingReference = null, Dictionary<string, string> data = null)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new Exception("GameObject must have a name");
+            }
+
+            if (string.IsNullOrWhiteSpace(type))
+            {
+                throw new Exception("GameObject must have a type");
+            }
+
+            return new GameObject(name, type, usingReference, data ?? new Dictionary<string, string>());
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # BG3ItemExtractor
+
 Extract armor, weapons and usable items from the uncompressed files of Baldur's Gate 3
+
+To use first uncompress Shared.pak and Gustav.Pak from Baldur's Gate 3 file folder which can be found at the following path
+C:\Program Files (x86)\Steam\steamapps\common\Baldurs Gate 3\Data
+
+You can use Norbyte's Extractor Tool to do that
+
+Once this is done, launch the executable, input the path f the uncompressed files and a destination folder
+This will generate Armor.txt, Weapon.txt and Item.txt
+A TreasureTable.txt will also be generated with the content of the three preceeding files
+
